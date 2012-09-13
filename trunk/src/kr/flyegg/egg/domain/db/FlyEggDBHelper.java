4e735ef3b24725595c16454a4c3f4a5cfa169d18
whdnfl21 <whdnfl21@b0578279-86f8-4dd4-9348-c64aac5292ac>
2012-09-13 09:59:56 +0000

    2012-09-13 update woori add db

---
+++ trunk/src/kr/flyegg/egg/domain/db/FlyEggDBHelper.java
@@ -0,0 +1,48 @@
+package kr.flyegg.egg.domain.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class FlyEggDBHelper extends SQLiteOpenHelper {
+
+	private static final String DATABASE_NAME = "flyegg";
+	private static final int DATABASE_VERSION = 6;
+	
+	
+	private String lock = "db_lock";
+	
+	public FlyEggDBHelper(Context context) {
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+	}
+	
+	public FlyEggDBHelper(Context context, String name, CursorFactory factory, int version) {
+		super(context, name, factory, version);
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		
+		synchronized (lock) {
+			db.execSQL("CREATE TABLE " + DBColumns.CARD_TABLE + "(" +
+					DBColumns.CARD_ID + " INTEGER PRIMARY KEY, " + 
+					DBColumns.CARD_TYPE + " INTEGER, " +  
+					DBColumns.CARD_DURATION+ " BIGINT, " +
+					//DBColumns.SERVER_MANAGE_APP_LIST_SOFTVERSION + " VARCHAR(50), " +
+					DBColumns.CARD_NUMBER + " TEXT, " + 
+					DBColumns.CARD_CREATEDDATE + " BIGINT);" );
+		}
+		
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		int version = oldVersion;
+		
+		if(version != DATABASE_VERSION) {
+			db.execSQL("DROP TABLE IF EXISTS " + DBColumns.CARD_TABLE);
+		}
+	}
+
+}
