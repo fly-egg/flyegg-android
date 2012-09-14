12a3c2a8a7987efc09102e3ed2a44c9bf36000e1
whdnfl21 <whdnfl21@b0578279-86f8-4dd4-9348-c64aac5292ac>
2012-09-14 19:09:17 +0000

    2012-09-15 update woori change package name

---
+++ trunk/src/kr/flyegg/egg/dao/Card.java
@@ -0,0 +1,107 @@
+package kr.flyegg.egg.dao;
+
+import android.graphics.Bitmap;
+
+public class Card {
+
+	private String word = null;
+	private String imgPath = null;
+	//일단 array 바뀔수도 있음.
+	private String category = null;
+	private String[] tags = null;
+	private Bitmap thumbnail = null;
+	
+	public Card() {
+	}
+	
+
+	/**
+	 * 
+	 * @param word
+	 * @param imgPath
+	 * @param category
+	 * @param tags
+	 */
+	public Card(String word, String imgPath, String category, String tags) {
+		this.word = word;
+		this.imgPath = imgPath;
+		this.category = category;
+		this.tags = tagToArray(tags);
+	}
+	
+	/**
+	 * 
+	 * @param word
+	 * @param imgPath
+	 * @param category
+	 * @param tags
+	 */
+	public Card(String word, String imgPath, String category, String[] tags) {
+		this.word = word;
+		this.imgPath = imgPath;
+		this.category = category;
+		this.tags = tags;
+	}
+
+	public Card(String word, String imgPath, String category, String[] tags, Bitmap thumbnail) {
+		this.word = word;
+		this.imgPath = imgPath;
+		this.category = category;
+		this.tags = tags;
+		this.thumbnail = thumbnail;
+	}
+	
+	public String getWord() {
+		return word;
+	}
+	public void setWord(String word) {
+		this.word = word;
+	}
+	public String getImgPath() {
+		return imgPath;
+	}
+	public void setImgPath(String imgPath) {
+		this.imgPath = imgPath;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String[] getTags() {
+		return tags;
+	}
+	
+	public String tagToString(String[] tags) {
+		StringBuffer sb = new StringBuffer();
+		for(String str : tags) {
+			sb.append(str).append(",");
+		}
+		return sb.toString();
+	}
+	
+	public String[] tagToArray(String tags) {
+		return tags.split(",");
+	}
+	
+	public void setTags(String[] tags) {
+		this.tags = tags;
+	}
+	public Bitmap getThumbnail() {
+		return thumbnail;
+	}
+	public void setThumbnail(Bitmap thumbnail) {
+		this.thumbnail = thumbnail;
+	}
+	
+	public String toString() {
+		StringBuffer sb = new StringBuffer();
+		sb.append("Card[word=").append(word).append(",imgPath=").append(imgPath);
+		sb.append(",category=").append(category).append(",tags=").append(tags);
+		sb.append("]");
+		
+		return sb.toString();
+	}
+	
+}
