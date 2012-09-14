cb4b364120607bd1c0401d166a4f1fe88eead041
woori <whdnfl21@gmail.com>
2012-09-14 19:09:03 +0000

    2012-09-15 update woori change package name

---
+++ src/kr/flyegg/egg/dao/db/FlyEggDBHelper.java
@@ -0,0 +1,60 @@
+package kr.flyegg.egg.dao.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class FlyEggDBHelper extends SQLiteOpenHelper {
+
+	private static final String DATABASE_NAME = "flyegg";
+	private static final int DATABASE_VERSION = 1;
+	
+	
+	private String lock = "db_lock";
+	
+	public FlyEggDBHelper(Context context) {
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+	}
+	
+	public FlyEggDBHelper(Context context, String name, CursorFactory factory, int version) {
+		super(context, name, factory, version);
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		
+		synchronized (lock) {
+			
+			db.execSQL("CREATE TABLE " + DBColumns.CARD_TABLE + "(" +
+				DBColumns.CARD_ID + " INTEGER PRIMARY KEY, " + 
+				DBColumns.CARD_WORD + " VARCHAR(100), " +  
+				DBColumns.CARD_IMGPATH + " VARCHAR(500), " + 
+				DBColumns.CARD_CATEGORY + " VARCHAR(500), " +
+				DBColumns.CARD_TAGS + " VARCHAR(500), " + 
+				//" BIGINT, " +
+				//DBColumns.SERVER_MANAGE_APP_LIST_SOFTVERSION + " VARCHAR(50), " +
+				//DBColumns.CARD_NUMBER + " TEXT, " + 
+				DBColumns.CARD_CREATEDDATE + " BIGINT);" );
+			
+			db.execSQL("CREATE TABLE " + DBColumns.CATEGORY_TABLE + "(" +
+					DBColumns.CATEGORY_ID + " INTEGER PRIMARY KEY, " + 
+					DBColumns.CATEGORY_NAME + " VARCHAR(100), " +  
+					DBColumns.CATEGORY_CREATEDDATE + " BIGINT);" );
+		
+		}
+		
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		int version = oldVersion;
+		
+		if(version != DATABASE_VERSION) {
+			db.execSQL("DROP TABLE IF EXISTS " + DBColumns.CARD_TABLE);
+			db.execSQL("DROP TABLE IF EXISTS " + DBColumns.CATEGORY_TABLE);
+		}
+	}
+
+}
+
