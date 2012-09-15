15d0c2637db0f4e1cb23808944974fe2ba85adab
woori <whdnfl21@gmail.com>
2012-09-15 03:58:26 +0000

    2012-09-15 update woori 갤러리랑 imageAdapter 분리했음~~

--- src/kr/flyegg/egg/ui/CardBoard.java
+++ src/kr/flyegg/egg/ui/CardBoard.java
@@ -1,21 +1,34 @@
 package kr.flyegg.egg.ui;
 
+
+import java.util.List;
+
 import kr.flyegg.egg.R;
+import kr.flyegg.egg.dao.Category;
+import kr.flyegg.egg.dao.CategoryAccesser;
+import kr.flyegg.egg.ui.tabview.PlusActivity;
+import kr.flyegg.egg.ui.tabview.TabViewActivity;
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.TabActivity;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.res.TypedArray;
 import android.graphics.Bitmap;
 import android.nfc.TagLostException;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnLongClickListener;
 import android.view.Window;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
+import android.widget.EditText;
 import android.widget.Gallery;
 import android.widget.GridView;
 import android.widget.ImageView;
@@ -32,7 +45,6 @@ public class CardBoard extends TabActivity implements TabHost.TabContentFactory 
 	private int count;
 	//sticker images array
 	private Bitmap[] thumbnails;
-	private ImageAdapter imageAdapter;
 	
 	
 	@Override
@@ -45,20 +57,28 @@ public class CardBoard extends TabActivity implements TabHost.TabContentFactory 
 		TabHost.TabSpec spec = null;
 	
 		
+		//무조건 들어가야하는 전체 탭
 		tabHost.addTab(tabHost.newTabSpec(getString(R.string.all_tab))
                 .setIndicator(getString(R.string.all_tab))
                 .setContent(this));
 		
-		
-		
 		//여기서부터 동적으로 동작하기
 		setCategory(tabHost);
 		
-		
 		tabHost.addTab(tabHost.newTabSpec(getString(R.string.plus))
                 .setIndicator(getString(R.string.plus))
-                .setContent(this));
+                .setContent(CardBoard.this));
+//                .setContent(new Intent(this, PlusActivity.class)));
 		
+		tabHost.getTabWidget().getChildAt(tabHost.getTabWidget().getChildCount()-1).setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				
+				if(dialog == null) {
+					//10 개 이상일 때 개수 세서 보여주기 toast 띄우던가
+					insertCategoryAlert();
+				}
+			}
+		});
 		
 		
 //		 for (int i=1; i <= 30; i++) {
@@ -77,34 +97,111 @@ public class CardBoard extends TabActivity implements TabHost.TabContentFactory 
 //		
 		tabHost.setCurrentTab( 0 );
 		
-		//size 정하기
+		
 		for(int tab=0; tab < tabHost.getTabWidget().getChildCount(); tab++) {
+			//size 정하기
 			tabHost.getTabWidget().getChildAt(tab).getLayoutParams().width = 150;
+
+			//0은 전체라서 지워지면 안된다.
+			if(tab != 0) {
+				//길게 누를 때 나오는 팝업 설정하기
+				tabHost.getTabWidget().getChildAt(tab).setOnLongClickListener(new OnLongClickListener() {
+					public boolean onLongClick(View v) {
+						
+						return false;
+					}
+				});
+			}
 		}
 		
 		
-		///////////////////////////////////////////////////////////////////////////////////
-		//sticker
-		Gallery g = (Gallery) findViewById(R.id.gallery);
-        // Set the adapter to our custom adapter (below)
-        g.setAdapter(new ImageAdapter(this));
-        
-        // Set a item click listener, and just Toast the clicked position
-        g.setOnItemClickListener(new OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
-                Toast.makeText(CardBoard.this, "" + position, Toast.LENGTH_SHORT).show();
-            }
-        });
-		
 	}
 
-    private void setCategory(TabHost tabHost2) {
+	
+	
+    @Override
+	protected void onStart() {
+		super.onStart();
+	}
+
+
+	private void setCategory(TabHost tabHost2) {
     	
     	//db에서 list를 가져온다.
+    	CategoryAccesser accesser = new CategoryAccesser(getApplicationContext());
+    	List<Category> list = accesser.getCategory();
     	
-		
+    	for(Category c : list) {
+    		
+    		Intent tab_intent = new Intent(CardBoard.this, TabViewActivity.class);
+    		tab_intent.putExtra(TabViewActivity.PROROSAL, c.getCategory());
+    		
+    		
+    		tabHost.addTab(tabHost.newTabSpec(c.getCategory())
+                    .setIndicator(c.getCategory())
+                    .setContent(tab_intent));
+//                    .setContent(new Intent(this, PlusActivity.class)));
+    	}
 	}
 
+	
+	private AlertDialog dialog = null;
+	
+	@SuppressLint("NewApi")
+	private void insertCategoryAlert() {
+		try {
+			
+		 LayoutInflater factory = LayoutInflater.from(this);
+         final View textEntryView = factory.inflate(R.layout.alert_dialog_text_entry, null);
+         dialog = new AlertDialog.Builder(CardBoard.this)
+             .setTitle(R.string.alert_dialog_text_entry)
+             .setView(textEntryView)
+             .setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+            	 
+                 public void onClick(DialogInterface dialog, int whichButton) {
+                	 EditText text = (EditText) textEntryView.findViewById(R.id.category_name_edit);
+                	 String a = text.getText().toString() ;
+                	 save(a);
+                 }
+             })
+             .setNegativeButton(R.string.no, new DialogInterface.OnClickListener() {
+                 public void onClick(DialogInterface dialog, int whichButton) {
+                	 //취소
+                	 dialog.cancel();
+                 }
+             })
+             .create();
+         
+         dialog.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	
+	@SuppressLint("NewApi")
+	private void save(String category) {
+		//save category
+		CategoryAccesser accesser = new CategoryAccesser(getApplicationContext());
+		accesser.insert(new Category(category));
+		
+		closeDialog();
+		
+		Intent intent = getIntent();
+		finish();
+		startActivity(intent);
+//		recreate();
+	}
+	
+
+	private void closeDialog() {
+		if(dialog != null) {
+			dialog.dismiss();
+			dialog = null;
+		}
+	}
+	
 	public View createTabContent(String tag) {
 //    	GridView imagegrid = (GridView) findViewById(R.id.gridview);
 //		imageAdapter = new ImageAdapter(this);
@@ -119,75 +216,7 @@ public class CardBoard extends TabActivity implements TabHost.TabContentFactory 
     
     
     
-	public class ImageAdapter extends BaseAdapter {
-//		private LayoutInflater mInflater;
 
-        private static final int ITEM_WIDTH = 136;
-        private static final int ITEM_HEIGHT = 88;
-
-        private final int mGalleryItemBackground;
-        private final Context mContext;
-
-        //Ư������ �ҷ� �� �� �ְ� �����ϱ�.. 
-        private final Integer[] mImageIds = {
-                R.drawable.gallery_photo_1,
-                R.drawable.gallery_photo_2,
-                R.drawable.gallery_photo_3,
-                R.drawable.gallery_photo_4,
-                R.drawable.gallery_photo_5,
-                R.drawable.gallery_photo_6,
-                R.drawable.gallery_photo_7,
-                R.drawable.gallery_photo_8
-        };
-
-        private final float mDensity;
-
-        public ImageAdapter(Context c) {
-            mContext = c;
-            // See res/values/attrs.xml for the <declare-styleable> that defines
-            // Gallery1.
-            TypedArray a = obtainStyledAttributes(R.styleable.gallery_sticker);
-            mGalleryItemBackground = a.getResourceId(R.styleable.gallery_sticker_android_galleryItemBackground, 0);
-            a.recycle();
-
-            mDensity = c.getResources().getDisplayMetrics().density;
-        }
-
-        public int getCount() {
-            return mImageIds.length;
-        }
-
-        public Object getItem(int position) {
-            return position;
-        }
-
-        public long getItemId(int position) {
-            return position;
-        }
-
-        public View getView(int position, View convertView, ViewGroup parent) {
-            ImageView imageView;
-            if (convertView == null) {
-                convertView = new ImageView(mContext);
-
-                imageView = (ImageView) convertView;
-                imageView.setScaleType(ImageView.ScaleType.FIT_XY);
-                imageView.setLayoutParams(new Gallery.LayoutParams(
-                        (int) (ITEM_WIDTH * mDensity + 0.5f),
-                        (int) (ITEM_HEIGHT * mDensity + 0.5f)));
-            
-                // The preferred Gallery item background
-                imageView.setBackgroundResource(mGalleryItemBackground);
-            } else {
-                imageView = (ImageView) convertView;
-            }
-
-            imageView.setImageResource(mImageIds[position]);
-
-            return imageView;
-        }
-	}
-        
 //		public ImageAdapter() {
 //			mInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 //		}
