8892af2887f6210e253192332c53bafef580202c
woori <whdnfl21@gmail.com>
2012-09-15 12:25:01 +0000

    2012-09-15 update woori flip

---
+++ src/kr/flyegg/egg/ui/event/SwapViews.java
@@ -0,0 +1,50 @@
+package kr.flyegg.egg.ui.event;
+
+import android.view.View;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SwapViews implements Runnable {
+
+	private boolean mIsFirstView;
+	ImageView image1;
+	TextView image2;
+
+	public SwapViews(boolean isFirstView, ImageView image1, TextView text1) {
+	 mIsFirstView = isFirstView;
+	 this.image1 = image1;
+	 this.image2 = text1;
+	}
+
+	public void run() {
+	 final float centerX = image1.getWidth() / 2.0f;
+	 final float centerY = image1.getHeight() / 2.0f;
+	 Flip3dAnimation rotation;
+
+	 if (mIsFirstView) {
+	  image1.setVisibility(View.GONE);
+	  image2.setVisibility(View.VISIBLE);
+	  image2.requestFocus();
+
+	     rotation = new Flip3dAnimation(-90, 0, centerX, centerY);
+	 } else {
+	  image2.setVisibility(View.GONE);
+	  image1.setVisibility(View.VISIBLE);
+	  image1.requestFocus();
+
+	     rotation = new Flip3dAnimation(90, 0, centerX, centerY);
+	 }
+
+	 rotation.setDuration(500);
+	 rotation.setFillAfter(true);
+	 rotation.setInterpolator(new DecelerateInterpolator());
+
+	 if (mIsFirstView) {
+	  image2.startAnimation(rotation);
+	 } else {
+	  image1.startAnimation(rotation);
+	 }
+	}
+	
+}
