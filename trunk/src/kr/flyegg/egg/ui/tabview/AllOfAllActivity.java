15d6e0d9484fbae06ec9f039d5209e04d678a0ec
whdnfl21 <whdnfl21@b0578279-86f8-4dd4-9348-c64aac5292ac>
2012-09-16 02:24:44 +0000

    2012-09-16 update woori 알럿~

--- trunk/src/kr/flyegg/egg/ui/tabview/AllOfAllActivity.java
+++ trunk/src/kr/flyegg/egg/ui/tabview/AllOfAllActivity.java
@@ -2,17 +2,27 @@ package kr.flyegg.egg.ui.tabview;
 
 
 import kr.flyegg.egg.R;
+import kr.flyegg.egg.ui.AddCardMain;
+import kr.flyegg.egg.ui.CardBoard;
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.ResolveInfo;
 import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.Checkable;
+import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.Gallery;
 import android.widget.GridView;
@@ -31,8 +41,64 @@ public class AllOfAllActivity extends Activity {
 		GridView g = (GridView) findViewById(R.id.myGrid);
         g.setAdapter(new AppsAdapter());
 		
+        Button plus = (Button) findViewById(R.id.tags_plus);
+        plus.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				insertCategoryAlert();
+			}
+		});
+        
+        Button select = (Button) findViewById(R.id.select);
+        select.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				closeDialog();
+				finish();
+			}
+		});
+        
 	}
 
+	
+	private void closeDialog() {
+		if(dialog != null) {
+			dialog.dismiss();
+			dialog = null;
+		}
+	}
+	
+	private AlertDialog dialog = null;
+	
+	@SuppressLint("NewApi")
+	private void insertCategoryAlert() {
+		try {
+			
+		 LayoutInflater factory = LayoutInflater.from(this);
+         final View textEntryView = factory.inflate(R.layout.alert_dialog_text_entry, null);
+         dialog = new AlertDialog.Builder(AllOfAllActivity.this)
+             .setTitle(R.string.alert_dialog_text_entry)
+             .setView(textEntryView)
+             .setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+            	 
+                 public void onClick(DialogInterface dialog, int whichButton) {
+                	 dialog.cancel();
+                 }
+             })
+             .setNegativeButton(R.string.no, new DialogInterface.OnClickListener() {
+                 public void onClick(DialogInterface dialog, int whichButton) {
+                	 //취소
+                	 dialog.cancel();
+                 }
+             })
+             .create();
+         
+         dialog.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
 	@Override
 	protected void onStart() {
 		super.onStart();
@@ -43,7 +109,8 @@ public class AllOfAllActivity extends Activity {
             R.drawable.grape,
             R.drawable.kiwi,
             R.drawable.pineapp,
-            R.drawable.watermelon
+            R.drawable.watermelon,
+            R.drawable.card_frame
     };
 	
     private final String[] mStrIds = {
@@ -51,8 +118,14 @@ public class AllOfAllActivity extends Activity {
             "포도",
             "키위",
             "파인애플",
-            "수박"
+            "수박",
+            "카드추가"
     };
+    
+    public void startaddCard() {
+    	Intent i = new Intent(getApplicationContext(), AddCardMain.class);
+    	startActivity(i);
+    }
     
 	public class AppsAdapter extends BaseAdapter {
         public AppsAdapter() {
@@ -88,6 +161,14 @@ public class AllOfAllActivity extends Activity {
 
             i.setImageResource(mImageIds[position]);
             tv.setText(mStrIds[position]);
+            if(position == 5) {
+            	tv.setOnClickListener(new OnClickListener() {
+					
+					public void onClick(View v) {
+						startaddCard();
+					}
+				});
+            }
             
             return l;
         }
